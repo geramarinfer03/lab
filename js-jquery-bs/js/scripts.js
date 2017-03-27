f362f559bfa5b4a18f2ec60310e5230568531306
ejemplo <geramarinfer03@gmail.com>
2017-03-27 15:04:44 -0600

    Segunda Parte Incompleta

--- js-jquery-bs/js/scripts.js
+++ js-jquery-bs/js/scripts.js
@@ -1,9 +1,6 @@
 var mostrarResultados = function(texto, estilo){
-	/*var elemento = document.getElementById('mostrarResultado');
-	elemento.innerHTML = texto;
-	elemento.className	 = estilo;*/
-	$('#mostrarResultado').text(texto);
-	$('#mostrarResultado').addClass(estilo)
+	$('#mostrarResultado').text(texto).attr('class',estilo);
+	//$('#mostrarResultado').addClass(estilo)
 }
 
 $('#btn_calcular_edad').click(function(){
@@ -12,8 +9,47 @@ $('#btn_calcular_edad').click(function(){
 	var fecha = $('#fecha_nacimiento').val();
 	var user = new usuario(nombre, fecha);
 	var texto = user._presentarse();
-	//mostrarResultados(texto, 'azul');
 });
+
+$('#btn_id_selector').click(function(){
+	var texto = $('#thing1').text();
+	mostrarResultados(texto, 'azul');
+
+});
+
+$('#btn_class_selector').click(function(){
+	var texto = $('.special').text();
+	mostrarResultados(texto, 'rojo');
+
+});
+
+$('#btn_tag_selector').click(function(){
+	var texto = $('h1').text();
+	mostrarResultados(texto, 'verde');
+});
+
+$('#btn_cambiarContenido').click(function(){
+	var texto1 = $('h1').text();
+	var texto2 = $('#thing1').text(); 
+	$('#thing1').append(texto1);
+	$('h1').html(texto2);
+
+	var texto3 = $('h1').text();
+	mostrarResultados(texto3, 'amarillo');
+});
+
+$('#btn_hijos').click(function(){
+	/*var texto1 = $('h1').text();
+	var texto2 = $('#thing1').text(); 
+	$('#thing1').html(texto1);
+	$('h1').html(texto2);
+
+	var texto3 = $('h1').text();
+	mostrarResultados(texto3, 'amarillo');*/
+
+});
+
+
 
 function usuario(nombre, fecha){
 	this._nombre = nombre;
@@ -34,11 +70,6 @@ function usuario(nombre, fecha){
 
     this._presentarse = function() {
  		
-       /*$('#mostrarResultado').text(
-           "Hola, mi nombre es " + this._nombre + 
-           " y tengo " + calcularEdad() + " años."
-        );
-       */ 
       var edad1 = calcularEdad();
         texto = "Hola, mi nombre es " + this._nombre + 
            " y tengo " + edad1 + " años.";
